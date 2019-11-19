36200559c068a9e8f786aef6c5c8aaefce78adc3
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2019-11-19 12:10:56 +0100

    Add immersive player mode

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ExoPlayerView.java
@@ -52,6 +52,13 @@ public final class ExoPlayerView extends FrameLayout {
     public ExoPlayerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         this.context = context;
 
         layoutParams = new ViewGroup.LayoutParams(
